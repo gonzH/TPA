603b4360ca6e206908bb630e8ac76f1abb83f5b5
Hellesandro Gonzaga <hellesandro@hotmail.com>
2019-06-17 21:46:29 -0300

    [REMASTER] main test for bfs() and dfs()

--- GrafoDirecionado/src/tadgrafod/Main.java
+++ GrafoDirecionado/src/tadgrafod/Main.java
@@ -17,25 +17,63 @@ public class Main {
         
         
         TADGrafoD tadGrafoD = new TADGrafoD("Grafozada");
-        tadGrafoD.insertVertex("A", 1);
-        tadGrafoD.insertVertex("B", 2);
-        tadGrafoD.insertVertex("C", 3);
-        tadGrafoD.insertVertex("D", 4);
-        tadGrafoD.insertVertex("E", 5);
-        tadGrafoD.insertVertex("F", 6);
-        tadGrafoD.insertVertex("G", 7);
+        tadGrafoD.insertVertex("A", 0);
+        tadGrafoD.insertVertex("B", 1);
+        tadGrafoD.insertVertex("C", 2);
+        tadGrafoD.insertVertex("D", 3);
+        tadGrafoD.insertVertex("E", 4);
+        tadGrafoD.insertVertex("F", 5);
+        tadGrafoD.insertVertex("G", 6);
+        tadGrafoD.insertVertex("H", 7);
         
         
+//        tadGrafoD.insertEdge("A", "B", "ab", 12);
+//        tadGrafoD.insertEdge("A", "C", "ac", 12);
+//        tadGrafoD.insertEdge("B", "D", "bd", 23);
+//        tadGrafoD.insertEdge("B", "E", "be", 33);
+//        tadGrafoD.insertEdge("B", "C", "bc", 33);
+//        tadGrafoD.insertEdge("C", "F", "cf", 44);
+//        tadGrafoD.insertEdge("C", "G", "cg", 44);
+//        tadGrafoD.insertEdge("E", "D", "ed", 55);
+//        tadGrafoD.insertEdge("F", "G", "fg", 44);
+
         tadGrafoD.insertEdge("A", "B", "ab", 12);
-        tadGrafoD.insertEdge("A", "C", "ac", 12);
-        tadGrafoD.insertEdge("B", "D", "bd", 23);
-        tadGrafoD.insertEdge("B", "E", "be", 33);
-        tadGrafoD.insertEdge("B", "C", "bc", 33);
-        tadGrafoD.insertEdge("C", "F", "cf", 44);
-        tadGrafoD.insertEdge("C", "G", "cg", 44);
-        tadGrafoD.insertEdge("E", "D", "ed", 55);
-        tadGrafoD.insertEdge("F", "G", "fg", 44);
+        tadGrafoD.insertEdge("A", "E", "ae", 23);
+        tadGrafoD.insertEdge("B", "C", "bc", 34);
+        tadGrafoD.insertEdge("B", "D", "bd", 45);
+        tadGrafoD.insertEdge("E", "F", "ef", 56);
+        tadGrafoD.insertEdge("E", "G", "eg", 67);
+        tadGrafoD.insertEdge("G", "H", "gh", 78);
+//        tadGrafoD.insertEdge("E", "B", "eb", 12);
+          
         
+        tadGrafoD.printmat();
+        
+        System.out.println("\n--------------------------------------------------\n");
+        
+        tadGrafoD.printgrafo();
+        
+        System.out.println("\n--------------------------------------------------\n");
+        
+//        tadGrafoD.removeVertex("A");
+//        tadGrafoD.removeVertex("G");
+//        tadGrafoD.removeVertex("E");
+//        
+//        tadGrafoD.printmat();
+//        
+//        System.out.println("\n--------------------------------------------------\n");
+//        
+//        tadGrafoD.printgrafo();
+//        
+//        System.out.println("\n--------------------------------------------------\n");
+//        tadGrafoD.removeEdge("ab");
+//        tadGrafoD.removeEdge("bc");
+//        tadGrafoD.removeEdge("bd");
+//        
+//        tadGrafoD.insertVertex("S", null);
+//        tadGrafoD.insertEdge("S", "F", "sf", 12);
+//        
+//
 //        tadGrafoD.printmat();
 //        
 //        System.out.println("\n--------------------------------------------------\n");
@@ -43,16 +81,18 @@ public class Main {
 //        tadGrafoD.printgrafo();
         
         
+        
+        
         ProcessaGrafo processaGrafo = new ProcessaGrafo(tadGrafoD);
-        LinkedList<Vertex> resultProcessa = processaGrafo.bsf("A");
+        LinkedList<Vertex> resultProcessa = processaGrafo.bfs("A");
         for(Vertex result : resultProcessa) {
             System.out.print(result.getLabel()+ " ");
         }
         System.out.println("");
-        
-//        LinkedList<Vertex> resultProcessa1 = processaGrafo.dsf("A");
-//        for(Vertex result : resultProcessa) {
-//            System.out.print(result.getLabel()+ " ");
-//        }
+      
+        LinkedList<Vertex> resultProcessa1 = processaGrafo.dfs("A");
+        for(Vertex result : resultProcessa1) {
+            System.out.print(result.getLabel()+ " ");
+        }
     }  
 }
